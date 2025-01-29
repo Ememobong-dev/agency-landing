e62ef8be2bd4087a8d4bb67fcfbccfcc14115583
Olushola <ememobongumoren2@gmail.com>
2025-01-29 17:07:32 +0100

    desktop design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,101 @@
 import Image from "next/image";
+import arrowDown from "../../public/images/icon-arrow-down.svg";
+import eggImg from "../../public/images/desktop/image-transform.jpg";
+import cupImg from "../../public/images/desktop/image-stand-out.jpg";
+import cherryImg from "../../public/images/desktop/image-graphic-design.jpg";
+import orange from "../../public/images/desktop/image-photography.jpg";
+import { Col, Row } from "antd";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="header_img flex flex-col">
+        <div className="flex flex-col gap-10 pt-44 justify-center items-center">
+          <h3 className="text-white my-5 font-frauncesRBold tracking-widest text-5xl uppercase ">
+            We are creatives
+          </h3>
+          <span>
+            <Image src={arrowDown} alt="arrow down" />
+          </span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <Row justify={"center"}>
+        <Col xs={22} lg={12}>
+          <div className="flex justify-center w-full h-full p-20 bg-white items-center">
+            <div className="flex flex-col">
+              <h3>Transform your brand</h3>
+              <p>
+                We are a full-service creative agency specializing in helping
+                brands grow fast. Engage your clients through compelling visuals
+                that do most of the marketing for you.
+              </p>
+              <p className="uppercase border-b-yellow  ">Learn More</p>
+            </div>
+          </div>
+        </Col>
+        <Col xs={22} lg={12}>
+          <span>
+            <Image src={eggImg} alt="" />
+          </span>
+        </Col>
+
+        <Col xs={22} lg={12}>
+          <span>
+            <Image src={cupImg} alt="" />
+          </span>
+        </Col>
+        <Col xs={22} lg={12}>
+          <div className="flex justify-center w-full h-full p-20 bg-white items-center">
+            <div className="flex flex-col">
+              <h3>Transform your brand</h3>
+              <p>
+                We are a full-service creative agency specializing in helping
+                brands grow fast. Engage your clients through compelling visuals
+                that do most of the marketing for you.
+              </p>
+              <p className="uppercase border-b-yellow  ">Learn More</p>
+            </div>
+          </div>
+        </Col>
+        <Col xs={22} lg={12}>
+          <div className="flex justify-center items-center">
+            <span className="relative">
+              <Image src={cherryImg} alt="" />
+            </span>
+            <div className="absolute  bottom-20">
+              <span className="flex flex-col justify-center text-center mx-auto">
+                <p className="text-darkDesaturatedCyan flex justify-center text-center font-frauncesSoftBold font-bold text-3xl ">
+                  Graphic Design
+                </p>
+                <span className="font-barlowReg font-bold my-5 text-darkDesaturatedCyan ">
+                  <p>Great design makes you memorable. We deliver</p>
+                  <p>artwork that underscores your brand message</p>
+                  <p>and captures potential clients’ attention.</p>
+                </span>
+              </span>
+            </div>
+          </div>
+        </Col>
+        <Col xs={22} lg={12}>
+          <div className="flex justify-center items-center">
+            <span className="relative">
+              <Image src={orange} alt="" />
+            </span>
+            <div className="absolute  bottom-20">
+              <span className="flex flex-col justify-center text-center mx-auto">
+                <p className="text-darkDesaturatedCyan flex justify-center text-center font-frauncesSoftBold font-bold text-3xl ">
+                  Photography
+                </p>
+                <span className="font-barlowReg font-bold my-5 text-darkDesaturatedCyan ">
+                  <p>Increase your credibility by getting the most</p>
+                  <p>stunning, high-quality photos that improve your</p>
+                  <p>business image.</p>
+                </span>
+              </span>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        veryDarkDesaturatedBlue: "hsl(212, 27%, 19%)",
+        veryDarkGrayishBlue: "hsl(213, 9%, 39%)",
+        darkGrayishBlue: "hsl(232, 10%, 55%)",
+        grayishBlue: "hsl(210, 4%, 67%)",
+        softRed: "hsl(7, 99%, 70%)",
+        yellow: "hsl(51, 100%, 49%)",
+        darkDesaturatedCyan: "hsl(167, 40%, 24%)",
+        darkBlue : "hsl(198, 62%, 26%)",
+        darkModerateCyan: "hsl(168, 34%, 41%)",
       },
+      fontFamily: {
+        barlowReg: ["barlow-regular", "sans-serif"],
+        barlowRBold: ["barlow-bold", "sans-serif"],
+        barlowLight: ["barlow-light", "sans-serif"],
+        frauncesReg: ["fraunces-regular", "sans-serif"],
+        frauncesBold: ["fraunces-bold", "sans-serif"],
+        frauncesLight: ["fraunces-light", "sans-serif"],
+        frauncesSoftBold: ["fraunces-soft-bold", "sans-serif"],
+      }
     },
   },
   plugins: [],
